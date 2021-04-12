6728e6c7d2e276d539bca427cd364a00e1c4d599
the next hokage <chanutpong.promsin@gmail.com>
2021-04-12 14:54:07 +0700

    change string -> int

---
+++ Models/AuthenModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabReservation.Models
+{
+    public class LoginModel
+    {
+        [RegularExpression("^(.+@.+[.].+|[aA]dmin)$", ErrorMessage = "รูปแบบ email ไม่ถูกต้อง")]
+        [Required(ErrorMessage = "กรุณากรอกข้อมูล")]
+        public string email { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "6 ถึง 50 ตัวอักษร", MinimumLength = 6)]
+        [Required(ErrorMessage = "กรุณากรอกข้อมูล")]
+        public string password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+
+        [RegularExpression("^(.+@.+[.].+|[aA]dmin)$", ErrorMessage = "รูปแบบ email ไม่ถูกต้อง")]
+        [Required(ErrorMessage = "กรุณากรอกข้อมูล")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "6 ถึง 50 ตัวอักษร", MinimumLength = 6)]
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string Password { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "รหัสผ่านไม่ตรงกัน")]
+        [StringLength(50, ErrorMessage = "6 ถึง 50 ตัวอักษร", MinimumLength = 6)]
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Models/ReturnModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabReservation.Models
+{
+    public class Return
+    {
+        public Boolean Error {get; set;}
+        public dynamic Data {get; set;}
+    }
+}
