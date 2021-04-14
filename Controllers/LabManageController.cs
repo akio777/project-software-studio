f2ed8b19d368eea5ae0563cf7dee42d1aa372106
Nachaphat Ainthong <mr.ntagfb@gmail.com>
2021-04-14 22:07:28 +0700

    add lab-manage-cancel page

--- Controllers/LabManageController.cs
+++ Controllers/LabManageController.cs
@@ -24,5 +24,22 @@ namespace LabReservation.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> EditCancel(int? id)
+        {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var labmanage_info = new LabManageCard();
+            labmanage_info.labinfo = await _context.Labinfo.FirstOrDefaultAsync(m => m.id == id);
+            labmanage_info.equipinfo = await _context.Equipment.FirstOrDefaultAsync(m => m.lab_id == id);
+
+            if (labmanage_info.labinfo == null || labmanage_info.equipinfo == null) {
+                return NotFound();
+            }
+
+            return View(labmanage_info);
+        }
     }
 }
