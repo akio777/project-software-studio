a0de07348edda86bce67de0191d12eec016c3581
the next hokage <chanutpong.promsin@gmail.com>
2021-04-14 17:46:45 +0700

    adding LabManageInfo Model

--- Models/LabinfoModel.cs
+++ Models/LabinfoModel.cs
@@ -18,4 +18,12 @@ namespace LabReservation.Models
         public string equip { get; set; }
         public int lab_id { get; set; }
     }
+
+    public class LabManageInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string equip { get; set; }
+        public int amount { get; set; }
+    }
 }
