f3640add31978110b3cf4451f0e9426068cf8613
the next hokage <chanutpong.promsin@gmail.com>
2021-04-16 15:47:59 +0700

    wrong type

--- Services/LabService.cs
+++ Services/LabService.cs
@@ -138,7 +138,7 @@ namespace LabReservation.Services
                 return new Return
                 {
                     Error = true,
-                    Data = "ไม่สามารถของได้ บัญชีของคุณถูกระงับ กรุณาติดต่อเจ้าหน้าที่"
+                    Data = "ไม่สามารถจองได้ บัญชีของคุณถูกระงับ กรุณาติดต่อเจ้าหน้าที่"
                 };
             }
             var dateNow = DateTime.Now;
